27108a65b5dafb265b616f0b51f7b47743ffa1e9
marcosdanielwindows <mteranc@unemi.edu.ec>
2024-03-06 18:34:20 -0500

    improve stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -28,7 +28,7 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
           },
         },
         quantity: 1,
@@ -68,7 +68,7 @@ const webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event.type === 'checkout.session.complete') {
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
   }
   res.status(200).json({ received: true });
