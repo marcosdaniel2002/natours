a5c3cc966f19f7bd312320ac3f3ad8ff499781cd
marcosdanielwindows <mteranc@unemi.edu.ec>
2024-03-06 17:08:48 -0500

    test stripe function

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -2,6 +2,7 @@ import { showAlert } from './alerts';
 
 export const bookTour = async function (tourId) {
   try {
+    console.log('Stripe process...');
     const stripe = Stripe(
       'pk_test_51OqRsPBUu4tqmoJIlXXsfYOg5of8iL1ATtdZOGg4oe3kqysw2JGcYdXmpIHWxD96OQOL1tN7FXmBSlKwNkZVU0us001jeJgx7d'
     );
